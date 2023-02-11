90fd53e3845a143816fbb1e473d1dd5f125d6b42
Era <picasoe51@gmail.com>
2023-02-11 20:57:35 +0330

    gen now makes level order trees (not really)

--- assets/Code/Python/generator.py
+++ assets/Code/Python/generator.py
@@ -3,35 +3,38 @@ import json
 import random
 
 
-def gen(crooms: dict, roomdata: dict, nroom: str, num: int, maxnum: int):
+def gen(crooms: dict, roomdata: dict, nroom: str, maxnum: int, pending: list, process: int):
+    num = len(crooms)
     crooms.setdefault(num, {})
     crooms[num]['text'] = roomdata[nroom]['text']
-    newnum = num
     roomdata[nroom].setdefault('choices', [])
-    for choice in roomdata[nroom]['choices']:
-        if newnum < maxnum:
-            newnum += 1
+    for i, choice in enumerate(roomdata[nroom]['choices']):
+        if num < maxnum:
             crooms[num].setdefault('choices', {})
             ranroom = random.choice(roomdata[nroom]['proutes'][choice])
-            crooms[num]['choices'][choice] = newnum
-            newnum = gen(crooms, roomdata, ranroom, newnum, maxnum)
-    return newnum
+            crooms[num]['choices'][choice] = len(crooms) + i
+            pending = gen(crooms, roomdata, ranroom, maxnum, pending, process + 1)
+    if process <= 0:
+        for i, v in enumerate(pending):
+            pending.pop(i)
+            gen(crooms, roomdata, v, maxnum, pending, process)
+    return pending
 
 
 name = input('What\'s gonna be the name of your cartridge? (str) ') or 'invalid name'
 try:
-    roomint = int(input('how many rooms would you like to be in your cartridge (not including the starter room)? (int) '))
+    roomint = int(input('how many rooms would you like to be in your cartridge? (int) ')) - 1
 except ValueError:
     roomint = 20
 print('opening rooms.json...')
-with open(os.path.abspath(os.getcwd()) + '/../Data/rooms.json') as f:
-    roomdata = json.loads(f.read())
+with open(os.path.abspath(os.getcwd()) + '/../../Data/rooms.json') as f:
+    data = json.loads(f.read())
 
 print('will generate rooms now...')
-starterroom = random.choice(roomdata['srooms'])
-crooms = {}
-gen(crooms, roomdata, starterroom, 0, roomint)
+starterroom = random.choice(data['srooms'])
+rooms = {}
+gen(rooms, data, starterroom, roomint, [], 0)
 
 with open(os.path.abspath(os.getcwd()) + '/' + name + '.json', 'w') as f:
-    f.write(json.dumps(crooms))
+    f.write(json.dumps(rooms))
 print('Done!')
