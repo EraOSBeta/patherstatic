6ef71a7fe1d97cc6e5a4b4faf39d8481ec1f6645
Era <picasoe51@gmail.com>
2023-02-14 16:34:18 +0330

    Menus are now not recursive (yay)

--- assets/Code/Python/player.py
+++ assets/Code/Python/player.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 import threading
 import keyboard
+import termios
 from time import sleep
 
 
@@ -11,6 +12,13 @@ def start_menu():
 
 
 def kex(btext: str | None, options: list, current: int, tcolor: str):
+    reprint(btext, options, current, tcolor)
+    sleep(0.1)
+    nc = [current]
+    wfkey(nc, 'up', 'down', btext, options, tcolor)
+
+
+def reprint(btext: str | None, options: list, current: int, tcolor: str):
     os.system('cls' if os.name == 'nt' else 'clear')
     if btext:
         print(btext + '\n\n')
@@ -19,34 +27,30 @@ def kex(btext: str | None, options: list, current: int, tcolor: str):
         if options[current] == option:
             hl = '\033[47m'
         print(hl + option + '\033[0m' + tcolor)
-    nc = [current]
-    dummy = threading.Thread(target=wfkey, args=[nc, 'up', 'down'])
-    sleep(0.1)
-    dummy.start()
-    dummy.join()
-    if nc[0] < 0:
-        current = len(options) - 1
-    elif nc[0] >= len(options):
-        current = 0
-    else:
-        current = nc[0]
-    kex(btext, options, current, tcolor)
 
 
-def wfkey(val, key1, key2):
+def wfkey(val: list | None = None, keyup: str | None = None, keydown: str | None = None, btext: str | None = None,
+          options: list | None = None, tcolor: str | None = None):
     while True:
-        if keyboard.is_pressed(key1):
+        if keyup and keyboard.is_pressed(keyup):
             val[0] -= 1
-            break
-        elif keyboard.is_pressed(key2):
+            if val[0] < 0:
+                val[0] = len(options) - 1
+            reprint(btext, options, val[0], tcolor)
+            sleep(0.1)
+        elif keydown and keyboard.is_pressed(keydown):
             val[0] += 1
+            if val[0] >= len(options):
+                val[0] = 0
+            reprint(btext, options, val[0], tcolor)
+            sleep(0.1)
+        elif keyboard.is_pressed('enter'):
             break
 
 
 print('This project requires your terminal to support a few things and follow a few standards, in order to check if '
       'your terminal has this standards, press "t" while in this menu or go to "options/teststd" in the main menu.\n'
       'I recommend you to NOT resize your window and let the game handle resizing.\n\npress "Enter" to continue')
-while True:
-    if keyboard.is_pressed('Enter'):
-        start_menu()
-        break
+wfkey()
+start_menu()
+termios.tcflush(0, termios.TCIFLUSH)
